944b64d811fb182df82bfd345104299ef03b464d
Sujel Ram <sujelram007@gmail.com>
2025-02-18 15:18:50 +0530

    saved

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -8,7 +8,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div id="container">
       <div id="d1">
-        <a id="a1" href="resources/0218 (1).mp4"><img id="image" src="resources/0218 (1).gif" alt="No image"></img>
+        <a id="a1" href="resources/download.gif"><img id="image" src="resources/download.gif" alt="No image"></img>
        <h5 id="text">Planet 3d Holographic image</h5></a>
       </div>
     </div>
