f183766f5c63dc6584661b28caac94f1259d28ae
Sujel Ram <sujelram007@gmail.com>
2025-02-18 14:46:38 +0530

    saved

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -11,7 +11,6 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
        <img id="image" src="resources/download.gif" alt="No image"></img>
        <h5 id="text">Planet 3d Holographic image</h5>
       </div>
-
     </div>
   );
 };
