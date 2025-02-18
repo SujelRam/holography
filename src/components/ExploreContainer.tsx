a04f0fcde179a9969dd78c14b16c9db6f9b5745d
Sujel Ram <sujelram007@gmail.com>
2025-02-18 16:22:53 +0530

    saved

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,44 @@
+import React, { useRef } from 'react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
 }
+
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleFullScreen = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if ((videoRef.current as any).webkitRequestFullscreen) { 
+        (videoRef.current as any).webkitRequestFullscreen();
+      } else if ((videoRef.current as any).mozRequestFullScreen) { 
+        (videoRef.current as any).mozRequestFullScreen();
+      } else if ((videoRef.current as any).msRequestFullscreen) { 
+        (videoRef.current as any).msRequestFullscreen();
+      }
+    }
+  };
   return (
     <div id="container">
-       <div id="d1">
-         <img id="image" src="resources/bg.png" alt="No image"></img>
-         <h5 id="text">Atom 3D Holography</h5>
-       </div>
+      <div id="d1">
+        <video
+          id="image"
+          ref={videoRef}
+          src="resources/Atom 3d animation.mp4"
+          loop
+          autoPlay
+          muted
+          controls
+          onClick={handleFullScreen}
+        ></video>
+        <h5 id="text">Atom 3D Holography</h5>
+      </div>
     </div>
   );
 };
 
 export default ExploreContainer;
+
