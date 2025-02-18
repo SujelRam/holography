519214ce399949b2283f8d5be0f7130672e54ed5
Sujel Ram <sujelram007@gmail.com>
2025-02-18 13:33:22 +0530

    saved

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -7,8 +7,11 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div id="container">
+      <div id="d1">
        <img id="image" src="resources/download.gif" alt="No image"></img>
-       <h5>Planet 3d Holographic image</h5>
+       <h5 id="text">Planet 3d Holographic image</h5>
+      </div>
+
     </div>
   );
 };
