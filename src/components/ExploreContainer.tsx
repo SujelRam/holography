374fdcbf7fe42929177aaaeff3a7fac47f7b2810
Sujel Ram <sujelram007@gmail.com>
2025-02-18 15:56:17 +0530

    saved

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -3,7 +3,6 @@ import './ExploreContainer.css';
 interface ContainerProps {
   name: string;
 }
-
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div id="container">
@@ -11,13 +10,6 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
          <img id="image" src="resources/bg.png" alt="No image"></img>
          <h5 id="text">Atom 3D Holography</h5>
        </div>
-
-       <div id="video-container">
-         <button id="back-button">Back</button>
-         <video id="video" src="resources/Atom 3d animation.mp4" loop controls></video>
-       </div>
-      <script src="components/video.js">
-      </script>
     </div>
   );
 };
--- src/components/video.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('d1').addEventListener('click', function() {
-   document.getElementById('video-container').style.display = 'block';
-   document.getElementById('video').play();
-});
-document.getElementById('back-button').addEventListener('click', function() {
-   document.getElementById('video-container').style.display = 'none';
-   document.getElementById('video').pause();
-});
